aa97b705a0f88d28c3bbfd8a71d5dc26738fae7c
savaha <vela369@gmail.com>
2026-05-15 16:57:27 -0500

    Update styles for card components and improve badge visibility in the album tracker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrementa VERSION cada vez que modifiques index.html, js/data.js o cualquier asset.
-const VERSION = 3;
+const VERSION = 4;
 const CACHE_NAME = `fullalbum-v${VERSION}`;
 const ASSETS = [
   './',
